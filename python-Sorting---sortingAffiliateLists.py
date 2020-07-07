c3b4c8f068c9fd0cb0ebacfbcb7cf8742f302d28
ZHIKANG TIAN <sdutbruce@outlook.com>
2020-07-07 23:38:27 +0800

    Update python-Sorting---sortingAffiliateLists.py

--- python-Sorting---sortingAffiliateLists.py
+++ python-Sorting---sortingAffiliateLists.py
@@ -15,30 +15,36 @@ NAT_ATP= [
 #define an lambda function as map callback:
 #  return a tuple with key of CW and value of a pair value of tuple/list
 map_callback_func = lambda x,y,z: (x,(y,z))
+
 #execute the lambda function as defined:
 #then get an Iterator(also is Iterable) object as result:
 #  parameters:   callback,     x,     y,    z
 r = map(map_callback_func, CW, plantATP, NAT_ATP)
+
 # get the list value of this iterator
 resultList = list(r);
+
 #then sort these tuple list by our lambda key:
 #  sorted by the integer at the first tuple element(key) string：
 #  e.g.: 'CW12' -> 12  /  "CW1" -> 1
 resultList.sort(key=lambda cw:int(cw[0][2:]))
+
 # then the list is sorted, and we can extract the affiliate array list
 # at the second element on these tuple(value):
 CW_list = []
 plantATP_list = []
 NAT_ATP_list = []
 for sorted_CW_tuple in resultList:
-    CW_list.append(sorted_CW_tuple[0])
-    plantATP_list.append(sorted_CW_tuple[1][0])
-    NAT_ATP_list.append(sorted_CW_tuple[1][1])
+    CW_list.append(sorted_CW_tuple[0]) #('CW1', (2, 22)) -> 'CW1'
+    plantATP_list.append(sorted_CW_tuple[1][0]) #('CW1', (2, 22)) -> (2,22)-> 2
+    NAT_ATP_list.append(sorted_CW_tuple[1][1])  #('CW1', (2, 22)) -> (2,22)-> 22
 
 print("CW_list="+str(CW_list))
 print("plantATP_list="+str(plantATP_list))
 print("NAT_ATP_list="+str(NAT_ATP_list))
 print("Origin CW="+str(CW))
+
+
 #In python 2.7: It's possible to define a customered comparable/sorting function
 #as one input parameter of sort() function:
 def comp(a1, a2):
@@ -49,11 +55,12 @@ def comp(a1, a2):
         return -1;
     else : #a1==a2
         return 0;
-
+     
 #The following are equivalent
 #1. define a key function as the sorting rule
 def key_callback_getCWNum(arr_ele):
     return int(arr_ele[2:])
 CW.sort(key=key_callback_getCWNum)
+
 #2. define lambda function as key
 CW.sort(key=lambda cw:int(cw[2:]))
